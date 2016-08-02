6027a1e25bdca9728516c5260ddaceec3b9e5f3e
Rich Churcher <rich.churcher@gmail.com>
2016-08-02 16:40:00 +1200

    Simple route example

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express')
+var development = require('../knexfile').development
+var knex = require('knex')(development)
 
 module.exports = {
   get: get
 }
 
 function get (req, res) {
-  ,w,
-  res.render('index', data)
+  knex('users')
+    .select()
+    .then(function (users) {
+      res.render('index', { users: users })
+    })
+    .catch(function (err) {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
 }
