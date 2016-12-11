50f689016152f370e5526d9acd663c161eee71c4
Don Smith <locksmithdon@gmail.com>
2016-12-12 01:27:45 +1300

    add migrations and seeds to test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,28 @@
 var test = require('tape')
 
-test('Working.', function (t) {
-  t.pass()
-  t.end()
+var config = require('../knexfile').test
+var testDb = require('knex')(config)
+
+function setupTestDb () {
+  process.env.NODE_ENV = 'test'
+  return testDb.migrate.latest()
+    .then(function () {
+      return testDb.seed.run()
+    })
+}
+
+function tearDownTestDb () {
+  process.env.NODE_ENV = 'development'
+  return testDb.destroy()
+}
+
+test('Test harness working', function (t) {
+  setupTestDb()
+    .then(runTest)
+    .then(tearDownTestDb)
+
+  function runTest () {
+    t.pass()
+    t.end()
+  }
 })
