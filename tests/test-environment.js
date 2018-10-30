4971693e0a105451b48ed07880db51519e2ddd93
Rich Churcher <rich.churcher@gmail.com>
2018-10-31 10:11:57 +1300

    Simplify

--- tests/test-environment.js
+++ tests/test-environment.js
@@ -1,21 +1,16 @@
-const getDbConn = require('knex')
+const knex = require('knex')
 
-const testConfig = require('../knexfile').test
+const config = require('../knexfile').test
 
 module.exports = {
   // Return a database connection for the test database
-  getTestDb: () => getDbConn(testConfig),
+  getTestDb: () => knex(config),
 
   // Create a separate in-memory database before each test
-  initialise: (db) => {
-    return db.migrate.latest()
-      .then(() => {
-        return db.seed.run()
-      })
-  },
+  initialise: db => db.migrate
+    .latest()
+    .then(() => db.seed.run()),
 
   // Close the database connection after each test
-  cleanup: (db) => {
-    return db.destroy()
-  }
+  cleanup: db => db.destroy()
 }
