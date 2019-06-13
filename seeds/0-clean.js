18b2615d579b4b5a900fac00492023edef89bc1f
Bryce Kehoe <bryce@kehoe.co.nz>
2019-06-14 09:45:35 +1200

    Fixed broken function call

--- seeds/0-clean.js
+++ seeds/0-clean.js
@@ -2,7 +2,7 @@ exports.seed = function (knex, Promise) {
   const empty = table =>
     () => knex(table).del()
 
-  return empty('users')
+  return empty('users')()
   // Chain calls to empty in
   // order as required, e.g.:
   // .then(empty('profiles'))
