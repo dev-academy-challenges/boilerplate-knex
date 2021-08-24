c022ca4f8c67c7e3b52a7abd834950aff39ce3ff
ysabel-guiang <ysabelregino@yahoo.com>
2021-08-25 09:36:17 +1200

    add connection to production in knexfile

--- knexfile.js
+++ knexfile.js
@@ -24,11 +24,10 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    connection: { 
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+  },
     pool: {
       min: 2,
       max: 10
