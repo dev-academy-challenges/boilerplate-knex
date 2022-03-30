d309998f4be11df136694e174161e6c3c8a31017
sarah-emerson <70471216+sarah-emerson@users.noreply.github.com>
2022-03-31 10:34:28 +1300

    Improve wording of clean seeds hint

--- seeds/0-clean.js
+++ seeds/0-clean.js
@@ -1,7 +1,8 @@
 exports.seed = (knex) => {
   return knex('users').del()
 
-  // Chain calls to empty in order as required, e.g.
+  // Delete data from child table(s) first, and use .then() to delete from parent tables
+  // e.g. to delete data from profiles and then from users:
   // return knex('profiles')
   //   .del()
   //   .then(() => knex('users').del())
